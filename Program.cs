61b65e0bf477f5321a595b071c126a5a5b0fd68d
Chaitra P <chaitra.p@bp.com>
2022-07-28 09:13:21 +0530

    Added UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Program");
+
+            EmployeeCheck.CheckEmployeeAttendence();
         }
     }
 }
